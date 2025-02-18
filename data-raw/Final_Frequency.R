0e28c2701d0587f37681208818af7894e7411787
Rene Gier Vemmelund <au612153@uni.au.dk>
2025-02-18 13:15:53 +0100

    Added taxonomy data on plants(including bryophytes) and lichens and joins accepted danish species names to Final_Frequency.

--- data-raw/Final_Frequency.R
+++ data-raw/Final_Frequency.R
@@ -9,13 +9,27 @@ Final_Frequency <- read_csv("data-raw/Final_Frequency.csv") |>
   dplyr::filter(!is.na(species)) |>
   dplyr::mutate(plot = as.character(plot))
 
+taxonlist_plants <- read_xlsx("data-raw/taxonlist_Plants.xlsx") %>%
+  select("Videnskabeligt navn", "Accepteret dansk navn") %>%
+  rename(Accepteret_dansk_navn = "Accepteret dansk navn")
+
+taxonlist_lichens <- read_xlsx("data-raw/taxonlist_Lichens.xlsx") %>%
+  select("Videnskabeligt navn", "Accepteret dansk navn") %>%
+  rename(Accepteret_dansk_navn = "Accepteret dansk navn")
+
+Final_Frequency <- Final_Frequency %>%
+  left_join(taxonlist_plants, by = join_by("species" == "Videnskabeligt navn")) %>%
+  left_join(taxonlist_lichens, by = join_by("species" == "Videnskabeligt navn")) %>%
+  mutate(Accepteret_dansk_navn = coalesce(Accepteret_dansk_navn.x, Accepteret_dansk_navn.y)) %>%
+  select(-Accepteret_dansk_navn.x, -Accepteret_dansk_navn.y)
+
 unique_taxa <- read_xlsx("data-raw/DFV_app_indicator_common_species.xlsx") %>%
   select(videnskabeligt_navn, taxon_id_Arter) %>%
   distinct()
 
 unique_taxa$videnskabeligt_navn <- tolower(unique_taxa$videnskabeligt_navn)
 
-taxa_df <- tibble(Taxa = Final_Frequency$Taxa)
+taxa_df <- tibble(Taxa = Final_Frequency$species)
 files_df <- tibble(Taxa = list.files("inst/Pictures"))
 
 # Standardize filenames by removing ".jpg" and replacing underscores with spaces
@@ -42,31 +56,19 @@ firstup <- function(x) {
 
 matched$videnskabeligt_navn <- firstup(matched$videnskabeligt_navn)
 
-# Update Final_Frequency with taxon_id_Arter and photo_file
-# Left join on Taxa and videnskabeligt_navn
-join1 <- Final_Frequency %>%
-  left_join(matched, by = c("Taxa" = "videnskabeligt_navn"))
-
 # Left join on species and videnskabeligt_navn
-join2 <- Final_Frequency %>%
+Final_Frequency <- Final_Frequency %>%
   left_join(matched, by = c("species" = "videnskabeligt_navn")) %>%
-  rename(Taxa = Taxa.x)
-
-# Combine the results
-Final_Frequency_combined <- bind_rows(join1, join2) %>%
-  distinct()  # Remove duplicate rows if any
-
-# Rename columns
-Final_Frequency_combined <- Final_Frequency_combined %>%
-  rename(photo_file = Taxa.y)
-
-Final_Frequency <- Final_Frequency_combined %>%
-  group_by(plot, year, Taxa) %>%
-  filter(!is.na(taxon_id_Arter) | row_number() == 1) %>%  # Keep non-NA taxon_id_Arter or the first row if all are NA
-  arrange(desc(!is.na(taxon_id_Arter))) %>%
-  slice(1) %>%  # Keep the first row within each group
-  ungroup() %>%  # Ungroup to return to the original data frame structure
+  rename(photo_file = Taxa) %>%
   distinct()
+
+# Final_Frequency <- Final_Frequency_combined %>%
+#   group_by(plot, year, Taxa) %>%
+#   filter(!is.na(taxon_id_Arter) | row_number() == 1) %>%  # Keep non-NA taxon_id_Arter or the first row if all are NA
+#   arrange(desc(!is.na(taxon_id_Arter))) %>%
+#   slice(1) %>%  # Keep the first row within each group
+#   ungroup() %>%  # Ungroup to return to the original data frame structure
+#   distinct()
 
 usethis::use_data(Final_Frequency, overwrite = TRUE)
 
