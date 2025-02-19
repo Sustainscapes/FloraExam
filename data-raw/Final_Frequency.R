2debf553bd43ef044c71d62e44ca49f2d8891bb9
Rene Gier Vemmelund <au612153@uni.au.dk>
2025-02-19 12:22:09 +0100

    Changes to frequency filtering to show only sphagnum and cladonia sp in relevant habtypes. Added taxon_lists from Arter.dk, changed taxon names and added filter on mosses.

--- data-raw/Final_Frequency.R
+++ data-raw/Final_Frequency.R
@@ -5,26 +5,53 @@ library(readxl)
 library(fuzzyjoin)
 library(stringdist)
 
+# This sources the apply_rules function for filtering the Final_Frequency dataset
+source("data-raw/Final_Frequency_rules.R")
+
 Final_Frequency <- read_csv("data-raw/Final_Frequency.csv") |>
+  # Run the apply_rules function on the species column
+  dplyr::mutate(species = map_chr(species, apply_rules)) |>
   dplyr::filter(!is.na(species)) |>
   dplyr::mutate(plot = as.character(plot))
 
-taxonlist_plants <- read_xlsx("data-raw/taxonlist_Plants.xlsx") %>%
-  select("Videnskabeligt navn", "Accepteret dansk navn") %>%
-  rename(Accepteret_dansk_navn = "Accepteret dansk navn")
+# Changing species names of Final_Frequency to match taxon names on Arter.dk
+Final_Frequency$species <- ifelse(Final_Frequency$species == "Taraxacum officinale",
+                                                 "Taraxacum officinale sensu lato", Final_Frequency$species)
+Final_Frequency$species <- ifelse(Final_Frequency$species == "Rubus fruticosus",
+                                  "Rubus fruticosus sensu lato", Final_Frequency$species)
+Final_Frequency$species <- ifelse(Final_Frequency$species == "Inula salicina",
+                                  "Pentanema salicinum", Final_Frequency$species)
+Final_Frequency$species <- ifelse(Final_Frequency$species == "Schoenoplectus maritimus",
+                                  "Bolboschoenus maritimus", Final_Frequency$species)
+Final_Frequency$species <- ifelse(Final_Frequency$species == "Avenula pratensis",
+                                  "Helictochloa pratensis", Final_Frequency$species)
+Final_Frequency$species <- ifelse(Final_Frequency$species == "Salicornia europaea coll.",
+                                  "Salicornia europaea", Final_Frequency$species)
+Final_Frequency$species <- ifelse(Final_Frequency$species == "Spartina ×townsendii",
+                                  "Spartina alterniflora × maritima", Final_Frequency$species)
 
-taxonlist_lichens <- read_xlsx("data-raw/taxonlist_Lichens.xlsx") %>%
-  select("Videnskabeligt navn", "Accepteret dansk navn") %>%
-  rename(Accepteret_dansk_navn = "Accepteret dansk navn")
+# Read in taxon data from Arter.dk. Excludes "Marchantiophyta", "Anthocerotophyta" and "Bryophyta" that are not Sphagnum.
+taxonlist_plants <- read_xlsx("data-raw/taxonlist_Plants.xlsx") |>
+  filter(!(Række == "Bryophyta" & Familie != "Sphagnaceae") & Række != "Marchantiophyta" & Række != "Anthocerotophyta") |>
+  dplyr::select("Videnskabeligt navn", "Accepteret dansk navn", "TaxonId") |>
+  rename(Accepteret_dansk_navn = "Accepteret dansk navn", taxon_id_Arter = "TaxonId")
+
+taxonlist_lichens <- read_xlsx("data-raw/taxonlist_Lichens.xlsx") |>
+  dplyr::select("Videnskabeligt navn", "Accepteret dansk navn", "TaxonId") |>
+  rename(Accepteret_dansk_navn = "Accepteret dansk navn", taxon_id_Arter = "TaxonId")
 
 Final_Frequency <- Final_Frequency %>%
   left_join(taxonlist_plants, by = join_by("species" == "Videnskabeligt navn")) %>%
   left_join(taxonlist_lichens, by = join_by("species" == "Videnskabeligt navn")) %>%
   mutate(Accepteret_dansk_navn = coalesce(Accepteret_dansk_navn.x, Accepteret_dansk_navn.y)) %>%
-  select(-Accepteret_dansk_navn.x, -Accepteret_dansk_navn.y)
+  mutate(taxon_id_Arter = coalesce(taxon_id_Arter.x, taxon_id_Arter.y)) |>
+  select(-Accepteret_dansk_navn.x, -Accepteret_dansk_navn.y, -taxon_id_Arter.x, -taxon_id_Arter.y)
 
-unique_taxa <- read_xlsx("data-raw/DFV_app_indicator_common_species.xlsx") %>%
-  select(videnskabeligt_navn, taxon_id_Arter) %>%
+# Remove records that did not match to Arter.dk taxa
+Final_Frequency <- Final_Frequency[!is.na(Final_Frequency$taxon_id_Arter),]
+
+unique_taxa <- read_xlsx("data-raw/DFV_app_indicator_common_species.xlsx") |>
+  select(videnskabeligt_navn) |>
   distinct()
 
 unique_taxa$videnskabeligt_navn <- tolower(unique_taxa$videnskabeligt_navn)
@@ -33,7 +60,7 @@ taxa_df <- tibble(Taxa = Final_Frequency$species)
 files_df <- tibble(Taxa = list.files("inst/Pictures"))
 
 # Standardize filenames by removing ".jpg" and replacing underscores with spaces
-files_df <- files_df %>%
+files_df <- files_df |>
   mutate(Taxa = gsub("\\.jpg$", "", Taxa), Taxa = gsub("\\.JPG$", "", Taxa),
          Taxa = gsub("_", " ", Taxa))
 
@@ -56,11 +83,16 @@ firstup <- function(x) {
 
 matched$videnskabeligt_navn <- firstup(matched$videnskabeligt_navn)
 
-# Left join on species and videnskabeligt_navn
-Final_Frequency <- Final_Frequency %>%
-  left_join(matched, by = c("species" = "videnskabeligt_navn")) %>%
-  rename(photo_file = Taxa) %>%
-  distinct()
+# Update Final_Frequency with taxon_id_Arter and photo_file
+Final_Frequency <- Final_Frequency |>
+  stringdist_left_join(
+    matched,
+    by = c("species" = "videnskabeligt_navn"),
+    method = "osa",
+    max_dist = 1
+  ) |>
+  rename(photo_file = Taxa) |>
+  select(-videnskabeligt_navn)
 
 # Final_Frequency <- Final_Frequency_combined %>%
 #   group_by(plot, year, Taxa) %>%
@@ -72,3 +104,5 @@ Final_Frequency <- Final_Frequency %>%
 
 usethis::use_data(Final_Frequency, overwrite = TRUE)
 
+# Cladonia and Sphagnum habitat codes are exported from the exceptions_and_rules.csv
+# file. These are the habitat types where we want to show these groups, else hide them.
